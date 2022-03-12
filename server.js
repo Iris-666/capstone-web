640734e7bf8fc3604b2e8cc931fac150284d30b0
Iris-666 <tq363@nyu.edu>
2022-03-12 15:56:23 +0800

    socketio

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-//hello
+var express = require('express')
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log("a user connected")
+})
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
