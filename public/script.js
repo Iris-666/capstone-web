64cef5187c07099cc7813a4572337d4ada3538ab
Iris-666 <tq363@nyu.edu>
2022-03-17 21:11:32 +0800

    something new

--- public/script.js
+++ public/script.js
@@ -1,2 +1,3 @@
 console.log('hi')
 let socket = io();
+let userLocation = {latitude: 31.25608, longtitude: 121.5755}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express')
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var https = require('https');
 
 app.use(express.static('public'))
 
@@ -19,6 +20,19 @@ io.on('connection', (socket) => {
 
 })
 
+https.get("https://www.openstreetmap.org/api/0.6/node/602360462/ways", (response)=>{
+    let data = ""
+    response.on("data", (chunk)=>{
+        data += chunk
+        // console.log(data)
+
+    })
+
+    response.on("end", ()=>{
+        console.log(data)
+    })
+})
+
 http.listen(8080, () => {
-    console.log('listening on *:3000');
+    console.log('listening on *:8080');
 });
