7237bbf26ba6ffc5d36dfa5b1bf8f4d161f00c85
Iris-666 <tq363@nyu.edu>
2022-03-17 23:28:18 +0800

    http request

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+const { response } = require('express');
 var express = require('express')
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var https = require('https');
+let userLocation = {latitude: 31.25608, longtitude: 121.5755}
+
 
 app.use(express.static('public'))
 
@@ -25,12 +28,23 @@ https.get("https://www.openstreetmap.org/api/0.6/node/602360462/ways", (response
     response.on("data", (chunk)=>{
         data += chunk
         // console.log(data)
+    })
 
+    response.on("end", ()=>{
+        // console.log(data)
+    })
+})
+
+https.get("https://api.openstreetmap.org/api/0.6/map?bbox=121.5750,31.25600,121.5760,31.25610", (response)=>{
+    let data = ""
+    response.on("data", (chunk)=>{
+        data += chunk
     })
 
     response.on("end", ()=>{
         console.log(data)
     })
+
 })
 
 http.listen(8080, () => {
